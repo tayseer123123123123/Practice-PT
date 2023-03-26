5e7e6ad827dd32d1b3def6ce30ea59cf16416433
tayseer123123123123 <89731780+tayseer123123123123@users.noreply.github.com>
2023-03-26 09:40:49 +0300

    Add transaction function added

--- # Make an empty list to store the transa.py
+++ # Make an empty list to store the transa.py
@@ -1,1 +1,13 @@
 # Make an empty list to store the transactions
+
+def add_transaction():
+    # Prompt the user for the transaction type income or expense and then the amount
+    transaction_t = input("Enter transaction type (income or expense): ")
+    amount = float(input("Enter amount: "))
+
+    # Add transaction to the list
+    transactions.append((transaction_t, amount))
+
+    # Print the confirmation message
+    print("Transaction added.")
+
